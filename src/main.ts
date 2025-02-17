b2349dc8495539800e16fc5f4d24df2ff6d2b4f7
Rajesh Nambi <rajeshnambi2016@gmail.com>
2025-02-17 23:17:44 +0530

    Vercel Analytics added

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { inject } from '@vercel/analytics';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+// Initialize Vercel Analytics
+inject();
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
