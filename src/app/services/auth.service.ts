dc6cb9249a711883afe9d709d22e05ddeb8c473e
Rajesh Nambi <rajeshnambi2016@gmail.com>
2025-04-26 03:42:31 +0530

    Update auth.service.ts

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -8,7 +8,8 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'https://snrcollege-h3h3ajhbbybbepfw.canadacentral-01.azurewebsites.net/api/auth/login';
+  private apiUrl = 'https://snreventmanagement-btfbc9brepeweabh.northeurope-01.azurewebsites.net/api/auth/login';
+  //private apiUrl = 'https://snrcollege-h3h3ajhbbybbepfw.canadacentral-01.azurewebsites.net/api/auth/login';
 
   constructor(private http: HttpClient) {}
 
